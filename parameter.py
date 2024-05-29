5c85af12f333d8f2a2da24e4bce9310368a434bf
ChenYuyan <790597778@qq.com>
2024-05-29 15:51:13 +0800

    add base model

--- parameter.py
+++ parameter.py
@@ -1,9 +1,9 @@
 import torch
-from model import DM2FNet, DM2FNet_woPhy
+from model import DM2FNet, DM2FNet_woPhy, DM2FNet_base, DM2FNet_woPhy_base
 
-model_R_Base = DM2FNet()
+model_R_Base = DM2FNet_base()
 model_R_Improve = DM2FNet()
-model_O_Base = DM2FNet_woPhy()
+model_O_Base = DM2FNet_woPhy_base()
 model_O_Improve = DM2FNet_woPhy()
 
 model_R_Base.load_state_dict(torch.load('model/RESIDE_ITS_baseline/new/iter_40000_loss_0.01225_lr_0.000000.pth'))
